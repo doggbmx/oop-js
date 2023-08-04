59d4235ad676f2e53af6431a032b865c6d1ce8d7
doggbmx <manuelorticosta@gmail.com>
2023-08-04 11:32:50 -0400

    end class 10

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+class Course {
+  constructor({ name, classes = [] }) {
+    this.name = name;
+    this.classes = classes;
+  }
+}
+const cursoProgBasica = new Course({
+  name: "Curso Gratis de Programación Básica",
+});
+const cursoDefinitivoHTML = new Course({
+  name: "Curso Definitivo de HTML y CSS",
+});
+
 // challenge
 class LearningPath {
   constructor({ name, courses = [] }) {
