c5ed7da1ce740448ecafe8d79165e8eb736687a8
doggbmx <manuelorticosta@gmail.com>
2023-08-04 10:40:11 -0400

    end class 6

--- main.js
+++ main.js
@@ -40,3 +40,42 @@ Student.prototype.aprobarCurso = function (nuevoCurso) {
 const juanita = new Student("juanita", 20, ["curso1", "curso2"]);
 // todos los metodos que agregamos con prototype se agregan al prototipo de la funcion constructora
 // es decir aparece en el atributo __proto__
+
+// Prototipos con la sintaxis de clases
+class Student2 {
+  contructor(name, age, cursosAprobados) {
+    this.name = name;
+    this.age = age;
+    this.cursosAprobados = cursosAprobados;
+  }
+  // creamos metodos
+  aprobarCurso(nuevoCurso) {
+    this.cursosAprobados.push(nuevoCurso);
+  }
+}
+
+const miguelito = new Student2("miguelito", 28, ["curso1", "curso2"]);
+
+// patron RORO [Recieve Object Return Object]
+// es un patron de diseño de funciones que reciben un objeto y retornan otro objeto con las modificaciones
+// que se le hicieron al objeto recibido
+class Student3 {
+  contructor({ name, age, email, twitter, cursosAprobados = [] }) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+    this.twitter = twitter;
+    this.cursosAprobados = cursosAprobados;
+  }
+  // creamos metodos
+  aprobarCurso(nuevoCurso) {
+    this.cursosAprobados.push(nuevoCurso);
+  }
+}
+
+const manu = Student3({
+  email: "coso@test.com",
+  age: 29,
+  name: "Manu",
+  twitter: "manu",
+});
