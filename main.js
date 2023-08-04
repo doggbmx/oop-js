cb637bba0c7d5ea464fc6d39d240cd112dea11b5
doggbmx <manuelorticosta@gmail.com>
2023-08-04 11:02:41 -0400

    end class 7

--- main.js
+++ main.js
@@ -1,81 +1,88 @@
-// Creamos un objeto LITERAL
-// los objetos literales no son instancia de un objeto en si, pero si del objeto Object
-const natalia = {
-  name: "Natalia",
-  age: 20,
-  cursosAprobados: [
+// challenge
+class LearningPath {
+  constructor({ name, courses = [] }) {
+    this.name = name;
+    this.courses = courses;
+  }
+} // EZ PZ
+const escuelaWeb = new LearningPath({
+  name: "Escuela de Desarrollo Web",
+  courses: [
     "Curso Definitivo de HTML y CSS",
-    "Curso Práctico de HTML y CSS",
+    "Curso Practico de HTML y CSS",
+    "Curso de Responsive Design",
   ],
-  // Creamos un método dentro del objeto
-  // hay 2 formas
-  //   aprobarCurso: () => {}
-  // estaforma es mas cheta
-  aprobarCurso(nuevoCurso) {
-    // hacemos referencia al atributo del objeto con this
-    this.cursosAprobados.push(nuevoCurso);
-  },
-};
-
-// Hacer q natalia apruebe otro curso
-// natalia.cursosAprobados.push("Curso de Responsive Design");
-
-// hacemos nuestro prototipo ahora
-// creamos una funcion constructora
-function Student(name, age, cursosAprobados) {
-  this.name = name;
-  this.age = age;
-  this.cursosAprobados = cursosAprobados;
-  // ahora te muestro una forma de crear un metodo
-  // this.aprobarCurso = (nuevoCurso) => {
-  //     this.cursosAprobados.push(nuevoCurso);
-  // }
-}
-// pero hay otra forma mas interesante
-Student.prototype.aprobarCurso = function (nuevoCurso) {
-  // esto es mas cheto la verdad
-  this.cursosAprobados.push(nuevoCurso);
-};
-// los prototipos se crean con la palabra reservada new
-const juanita = new Student("juanita", 20, ["curso1", "curso2"]);
-// todos los metodos que agregamos con prototype se agregan al prototipo de la funcion constructora
-// es decir aparece en el atributo __proto__
-
-// Prototipos con la sintaxis de clases
-class Student2 {
-  contructor(name, age, cursosAprobados) {
-    this.name = name;
-    this.age = age;
-    this.cursosAprobados = cursosAprobados;
-  }
-  // creamos metodos
-  aprobarCurso(nuevoCurso) {
-    this.cursosAprobados.push(nuevoCurso);
-  }
-}
-
-const miguelito = new Student2("miguelito", 28, ["curso1", "curso2"]);
-
-// patron RORO [Recieve Object Return Object]
-// es un patron de diseño de funciones que reciben un objeto y retornan otro objeto con las modificaciones
-// que se le hicieron al objeto recibido
-class Student3 {
-  contructor({ name, age, email, twitter, cursosAprobados = [] }) {
-    this.name = name;
-    this.age = age;
-    this.email = email;
-    this.twitter = twitter;
-    this.cursosAprobados = cursosAprobados;
-  }
-  // creamos metodos
-  aprobarCurso(nuevoCurso) {
-    this.cursosAprobados.push(nuevoCurso);
-  }
-}
-
-const manu = Student3({
-  email: "coso@test.com",
-  age: 29,
-  name: "Manu",
-  twitter: "manu",
 });
+
+// utilizaremos clases
+class Student {
+  constructor({
+    name,
+    username,
+    email,
+    twitter = undefined,
+    instagram = undefined,
+    facebook = undefined,
+    approvedCourses = [],
+    learningPaths = [],
+  }) {
+    this.name = name;
+    this.username = username;
+    this.email = email;
+    this.socialMedia = { twitter, instagram, facebook };
+    this.approvedCourses = approvedCourses;
+    this.learningPaths = learningPaths;
+  }
+}
+
+const juan2 = new Student({
+  name: "JuanDC",
+  username: "juandc",
+  email: "juan@juan.com",
+  twitter: "fjuandc",
+  // implemento el challenge
+  learningPaths: [escuelaWeb],
+});
+
+const miguel = new Student({
+  name: "Miguel",
+  username: "miguel",
+  email: "mi@guel.com",
+  instagram: "miguel",
+  facebook: "miguel",
+  approvedCourses: ["Curso de Responsive Design"],
+});
+
+// crear un objeto literal fue exageradamente mucho trabajo.
+// const juan1 = {
+//   name: "juan",
+//   username: "juan1",
+//   points: 100,
+//   socialMedia: {
+//     twitter: "fjuandc",
+//     instagram: "fjuandc",
+//     facebook: undefined,
+//   },
+//   approvedCourses: [
+//     "Curso Definitivo de HTML y CSS",
+//     "Curso Práctico de HTML y CSS",
+//   ],
+//   learningPaths: [
+//     {
+//       name: "Escuela de desarrollo Web",
+//       courses: [
+//         "Curso Definitivo de HTML y CSS",
+//         "Curso Práctico de HTML y CSS",
+//         "Curso de Responsive Design",
+//       ],
+//     },
+//     {
+//       name: "Escuela de Videojuegos",
+//       courses: [
+//         "Curso de Introducción a la Producción de Videojuegos",
+//         "Curso de Unreal Engine",
+//         "Curso de Unity 3D",
+//       ],
+//     },
+//   ],
+// };
